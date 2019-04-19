0d7a2e8ae9db6b903503d94d793acb9ffc8136d0
hilfmir <nicorome69@gmail.com>
2019-04-18 18:30:54 -0700

    Create Sqrt.py

---
+++ Sqrt.py
@@ -0,0 +1,17 @@
+import arcpy, numpy
+arcpy.env.workspace = r""
+
+fc = "LandParcels"
+query = 'Zone = \'Zone D\''
+
+x = arcpy.da.FeatureClassToNumPyArray(aFC, "TaxValue06", query)
+n = [numpy.square(i) for i in x['TaxValue06']]
+    sum = numpy.sum(n)
+    sqrt = numpy.sqrt(sum)
+    items = [(i / sqrt) for i in x['TaxValue06']]
+    
+    for i in items:
+        print i
+
+
+
